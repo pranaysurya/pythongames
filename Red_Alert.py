793e5ac416c2ff863996a250e1f48428561c67c2
pranaysurya <pranaysuryaisthebest@gmail.com>
2022-04-03 14:15:12 -0400

    added work

--- Red_Alert.py
+++ Red_Alert.py
@@ -1,1 +1,59 @@
-print('hello')
+# author : Pranay
+# Purpose:
+
+import random
+
+FONT_COLOUR = (255, 255, 255)
+WIDTH = 800
+HEIGHT = 600
+CENTER_X = WIDTH / 2
+CENTER_Y = HEIGHT / 2
+CENTER = (CENTER_X, CENTER_Y)
+FINAL_LEVEL = 6
+START_SPEED = 10
+COLORS = ["green", "blue"]
+
+game_over = False
+game_complete = False
+current_level = 1
+stars = []
+animations = []
+
+def draw():
+    global stars, current_level, game_over, game_complete
+    screen.clear()
+    screen.blit("space", 0, 0)
+    if game_over:
+        display_message("GAME OVER!", "Try again.")
+    elif game_complete:
+        display_message("YOU WON!", "Well done.")
+    else:
+        for star in stars:
+            star.draw
+
+def update():
+    global stars
+    if len(stars) == 0:
+        stars = make_stars(current_level)
+
+def make_stars(number_of_extra_stars):
+    colors_to_create = get_colors_to_create(number_of_extra_stars)
+    new_stars = create_stars(colors_to_create)
+    layout_stars(new_stars)
+    animate_stars(new_stars)
+    return new_stars
+
+def get_colors_to_create(number_of_extra_stars):
+    colors_to_create = ["red"]
+    for i in range(0, number_of_extra_stars):
+        random_color = random.choice(COLORS)
+        colors_to_create.append(random_color)
+    return colors_to_create
+
+def create_stars(colors_to_create):
+    new_stars  = 1
+    for colors_to_create:
+        star = Actor(color + "-star")
+        new_stars.append(star)
+    return new_stars
+
