4f02c0d4aa564c4fb47734a67aedd91706514cc0
pranaysurya <pranaysuryaisthebest@gmail.com>
2022-04-24 12:11:05 -0400

    changed to use file and remove hardcoded questions, Quiz.txt Coder = Pranay Surya Addagarla

---
+++ Happy_Garden.py
@@ -0,0 +1,81 @@
+from random import randint
+import time
+
+WIDTH = 800
+HEIGHT = 600
+CENTER_X = WIDTH / 2
+CENTER_Y = HEIGHT / 2
+
+game_over = False
+finalized = False
+garden_happy = True
+fangflower_collision = False
+
+time_elapsed = 0
+start_time = time.time()
+
+cow = Actor("cow")
+cow.pos = 100, 500
+
+flower_list = []
+wilted_list = []
+fangflower_list = []
+
+fangflower_vx_list = []
+fangflower_vy_list = []
+
+def draw():
+    global game_over, time_elapsed, finalized
+    if not game_over:
+        screen.clear()
+        screen.blit("garden", (0, 0))
+        cow.draw()
+        for flower in flower_list:
+            flower.draw()
+        for fangflower in fangflower_list:
+            fangflower.draw()
+        time_elapsed = int(time.time() - start_time)
+        screen.draw.text("Garden happy for: " +)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
